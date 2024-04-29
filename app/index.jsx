671278bf5d4d0060fb67bd3252309302cca4b536
Leo1526 <mustafatanriyakul1@gmail.com>
2024-04-29 21:14:32 +0300

    yapıldı

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-pblack">hacep</Text>
       <StatusBar style='auto' />
-      <Link href="/sign-in" style={{color: 'blue'}} >Go to Sign In</Link>
+      <Link href="/sign-in" style={{color: 'blue'}} >Go to Salih</Link>
     </View>
   )
 }
