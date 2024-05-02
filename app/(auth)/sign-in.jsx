80e7808a8a88ff0f4c612d4e19326fb77842a441
Kazuchi Takemi <kazuvhi@gmail.com>
2024-05-02 23:07:01 +0300

    changes are made

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { TextInput, Button, Snackbar, Text } from 'react-native-paper';
+import { View, StyleSheet, Image, KeyboardAvoidingView, Platform, ScrollView, Text } from 'react-native';
+import { TextInput, Button, Snackbar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-import { colors,fonts } from '../../design/themes';
+import { colors, fonts } from '../../design/themes';
+
 const LoginScreen = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -12,84 +13,80 @@ const LoginScreen = () => {
 
   const handleLogin = () => {
     if (username === 'admin' && password === 'admin123') {
-      // Kullanıcı doğrulandı, burada giriş işlemini gerçekleştirin
       console.log('Giriş başarılı!');
     } else {
-      // Kullanıcı doğrulanamadı, hata mesajı göster
       setSnackbarVisible(true);
     }
   };
 
   return (
-    <View style={styles.container}>
-      {/* Logo ve uygulama adı */}
-      <Image
-        source={require('../../assets/images/logo.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />
-      <Text style={styles.appName}>Uygulama Adı</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require('../../assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <Text style={styles.appName}>Uygulama Adı</Text>
 
-      <TextInput
-        label="Kullanıcı Adı"
-        value={username}
-        onChangeText={text => setUsername(text)}
-        selectionColor= {colors.primary}
-        activeUnderlineColor= {colors.primary}
-        style={styles.input}
-        theme={{ colors: { primary: 'blue' } }}
-      />
-      <TextInput
-        label="Parola"
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry={!showPassword}
-        style={styles.input}
-        selectionColor= {colors.primary}
-        activeUnderlineColor= {colors.primary}
-        theme={{ colors: { primary: 'blue' } }}
-        right={<TextInput.Icon icon={showPassword ? 'eye-off' : 'eye'} color= {colors.primary} onPress={() => setShowPassword(!showPassword)} />}
-      />
-      <Button
-        mode="contained"
-        onPress={handleLogin}
-        style={styles.button}
-        buttonColor= {colors.primary}
-      >
-        Giriş Yap
-      </Button>
-      <Snackbar
-        visible={snackbarVisible}
-        onDismiss={() => setSnackbarVisible(false)}
-        duration={3000}
-        action={{
-          label: 'Tamam',
-          onPress: () => setSnackbarVisible(false),
-        }}
-      >
-        Kullanıcı adı veya parola hatalı
-      </Snackbar>
+        <TextInput
+          label="Kullanıcı Adı"
+          value={username}
+          onChangeText={text => setUsername(text)}
+          selectionColor={colors.primary}
+          activeUnderlineColor={colors.primary}
+          style={styles.input}
+          theme={{ colors: { primary: 'blue' } }}
+        />
+        <TextInput
+          label="Parola"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          secureTextEntry={!showPassword}
+          style={styles.input}
+          selectionColor={colors.primary}
+          activeUnderlineColor={colors.primary}
+          theme={{ colors: { primary: 'blue' } }}
+          right={<TextInput.Icon icon={showPassword ? 'eye-off' : 'eye'} color={colors.primary} onPress={() => setShowPassword(!showPassword)} />}
+        />
+        <Button
+          mode="contained"
+          onPress={handleLogin}
+          style={styles.button}
+          buttonColor={colors.primary}
+        >
+          Giriş Yap
+        </Button>
+        <Snackbar
+          visible={snackbarVisible}
+          onDismiss={() => setSnackbarVisible(false)}
+          duration={3000}
+          action={{
+            label: 'Tamam',
+            onPress: () => setSnackbarVisible(false),
+          }}
+        >
+          Kullanıcı adı veya parola hatalı
+        </Snackbar>
 
-      <View style={styles.textContainer}>
-      <Text style={styles.signupButtonText}>Hesabın yok mu? {'\u00A0'}</Text>
-      <Button
-        onPress={() => navigation.navigate('sign-up')}
-        labelStyle={styles.signupButtonText}
-        buttonColor= {colors.secondary}
-      >
-         Kayıt Ol
-      </Button>
-      </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.signupButtonText}>Hesabın yok mu? {'\u00A0'}</Text>
+          <Button
+            onPress={() => navigation.navigate('sign-up')}
+            labelStyle={styles.signupButtonText}
+            buttonColor={colors.secondary}
+          >
+            Kayıt Ol
+          </Button>
+        </View>
 
-      {/* Developer Team bilgisi */}
-      <Text style={styles.developerText}>Developed by Developer Team</Text>
-    </View>
+        <Text style={styles.developerText}>Developed by Developer Team</Text>
+      </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
@@ -113,10 +110,6 @@ const styles = StyleSheet.create({
     width: '100%',
     marginBottom: 16,
   },
-  signupButton: {
-    backgroundColor: 'transparent',
-    marginBottom: 16,
-  },
   developerText: {
     position: 'absolute',
     bottom: 16,
@@ -125,8 +118,6 @@ const styles = StyleSheet.create({
   textContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    color :"red",
-    
   },
   signupButtonText: {
     color: colors.text,
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { TextInput, Button, Snackbar, Text } from 'react-native-paper';
-import { Picker } from '@react-native-picker/picker';
+import { View, StyleSheet, Image, ScrollView, KeyboardAvoidingView, Platform, Text } from 'react-native';
+import { TextInput, Button, Snackbar } from 'react-native-paper';
+import RNPickerSelect from 'react-native-picker-select';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../../design/themes';
-import RNPickerSelect from 'react-native-picker-select';
+
 const SignUp = () => {
   const [email, setEmail] = useState('');
   const [university, setUniversity] = useState('');
@@ -12,151 +12,126 @@ const SignUp = () => {
   const [surname, setSurname] = useState('');
   const [studentId, setStudentId] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const [snackbarVisible, setSnackbarVisible] = useState(false);
   const navigation = useNavigation();
-  const [showPassword, setShowPassword] = useState(false);
-
 
   const handleSignUp = () => {
-    // Kayıt işlemleri burada gerçekleştirilir
     console.log('Kayıt başarılı!');
   };
+
   const schools = [
     { label: 'Okul 1', value: 'Okul 1' },
     { label: 'Okul 2', value: 'Okul 2' },
     { label: 'Okul 3', value: 'Okul 3' },
   ];
+
   return (
-    <View style={styles.container}>
-      {/* Logo ve uygulama adı */}
-      <Image
-        source={require('../../assets/images/logo.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />
-      <Text style={styles.appName}>Uygulama Adı</Text>
+   
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require('../../assets/images/logo.png')}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+        <Text style={styles.appName}>Uygulama Adı</Text>
 
-      <RNPickerSelect
-        onValueChange={(value) => console.log(value)}
-        items={schools}
-        style={{
-          inputIOS: styles.input,
-          inputAndroid: styles.input,
-        }}
-        placeholder={{
-          label: 'Bir okul seçin...',
-          value: null,
-        }}
-        value={schools}
-      />
-      {/* <Picker
-        selectedValue={schools}
-        onValueChange={(itemValue, itemIndex) => setSchool(itemValue)}
-        style={[styles.input]}
-      >
-        {schools.map((school, index) => (
-          <Picker.Item key={index} label={school.label} value={school.value} />
-        ))}
-      </Picker> */}
+        <RNPickerSelect
+          onValueChange={(value) => setUniversity(value)}
+          items={schools}
+          style={{
+            inputIOS: styles.input,
+            inputAndroid: styles.input,
+          }}
+          placeholder={{
+            label: 'Bir okul seçin...',
+            value: null,
+          }}
+          value={university}
+        />
 
-      <TextInput
-        label="Email"
-        value={email}
-        onChangeText={text => setEmail(text)}
-        style={styles.input}
-        theme={{ colors: { primary: colors.primary } }}
-        keyboardType="email-address"
-      />
-      
-      <TextInput
-        label="İsim"
-        value={name}
-        onChangeText={text => setName(text)}
-        style={styles.input}
-        theme={{ colors: { primary: colors.primary } }}
-      />
-      <TextInput
-        label="Soyisim"
-        value={surname}
-        onChangeText={text => setSurname(text)}
-        style={styles.input}
-        theme={{ colors: { primary: colors.primary } }}
-      />
-      <TextInput
-        label="Öğrenci Numarası"
-        value={studentId}
-        onChangeText={text => setStudentId(text)}
-        style={styles.input}
-        theme={{ colors: { primary: colors.primary } }}
-        keyboardType="numeric"
-      />
-      <TextInput
-        label="Parola"
-        value={password}
-        onChangeText={text => setPassword(text)}
-        secureTextEntry={!showPassword}
-        style={styles.input}
-        selectionColor= {colors.primary}
-        activeUnderlineColor= {colors.primary}
-        theme={{ colors: { primary: 'blue' } }}
-        right={<TextInput.Icon icon={showPassword ? 'eye-off' : 'eye'} color= {colors.primary} onPress={() => setShowPassword(!showPassword)} />}
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+          keyboardType="email-address"
+        />
+        <TextInput
+          label="İsim"
+          value={name}
+          onChangeText={text => setName(text)}
+          style={styles.input}
+        />
+        <TextInput
+          label="Soyisim"
+          value={surname}
+          onChangeText={text => setSurname(text)}
+          style={styles.input}
+        />
+        <TextInput
+          label="Öğrenci Numarası"
+          value={studentId}
+          onChangeText={text => setStudentId(text)}
+          style={styles.input}
+          keyboardType="numeric"
+        />
+        <TextInput
+          label="Parola"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          secureTextEntry={!showPassword}
+          style={styles.input}
+          right={<TextInput.Icon icon={showPassword ? 'eye-off' : 'eye'} onPress={() => setShowPassword(!showPassword)} />}
+        />
+        <Button
+          mode="contained"
+          onPress={handleSignUp}
+          style={styles.button}
+        >
+          Kayıt Ol
+        </Button>
 
-      />
-      <Button
-        mode="contained"
-        onPress={handleSignUp}
-        style={styles.button}
-        buttonColor={colors.primary}
-      >
-        Kayıt Ol
-      </Button>
-      <Snackbar
-        visible={snackbarVisible}
-        onDismiss={() => setSnackbarVisible(false)}
-        duration={3000}
-        action={{
-          label: 'Tamam',
-          onPress: () => setSnackbarVisible(false),
-        }}
-      >
-        Kayıt olma işlemi başarısız oldu.
-      </Snackbar>
+        <Snackbar
+          visible={snackbarVisible}
+          onDismiss={() => setSnackbarVisible(false)}
+          duration={3000}
+          action={{
+            label: 'Tamam',
+            onPress: () => setSnackbarVisible(false),
+          }}
+        >
+          Kayıt olma işlemi başarısız oldu.
+        </Snackbar>
 
-      {/* Giriş sayfasına yönlendirme butonu */}
-      <View style={styles.textContainer}>
-      <Text style={styles.signupButtonText}>Hesabın yok mu? {'\u00A0'}</Text>
-      <Button
-        onPress={() => navigation.navigate('sign-up')}
-        labelStyle={styles.signupButtonText}
-        buttonColor= {colors.secondary}
-      >
-         Kayıt Ol
-      </Button>
-      </View>
+        <View style={styles.textContainer}>
+          <Text style={styles.signupButtonText}>Hesabın yok mu? {'\u00A0'}</Text>
+          <Button
+            onPress={() => navigation.navigate('sign-in')}
+            labelStyle={styles.signupButtonText}
+            buttonColor={colors.secondary}
+          >
+            Kayıt Ol
+          </Button>
+        </View>
 
-      {/* Developer Team bilgisi */}
-      <Text style={styles.developerText}>Developed by Developer Team</Text>
-    </View>
+        <Text style={styles.developerText}>Developed by Developer Team</Text>
+      </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 16,
     backgroundColor: colors.background,
   },
-  textContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    color :"red",
-    
-  },
   logo: {
     width: 100,
     height: 100,
+    marginBottom: 20,
   },
   appName: {
     fontSize: 24,
@@ -167,24 +142,26 @@ const styles = StyleSheet.create({
   input: {
     width: '100%',
     marginBottom: 16,
-    backgroundColor: colors.text,
-    borderRadius : 8,
-    
+    backgroundColor: 'white',
+    borderRadius: 8,
   },
   button: {
     width: '100%',
-    marginBottom: 16,
+    padding: 10,
+    backgroundColor: colors.primary,
   },
-  signupButton: {
-    backgroundColor: 'transparent',
-    marginBottom: 16,
+  textContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
   },
   signupButtonText: {
-    color: colors.primary,
+    color: colors.text,
+    fontWeight: 'bold',
   },
   developerText: {
     position: 'absolute',
-    bottom: 16,
+    bottom: 10,
     color: colors.text,
   },
 });
