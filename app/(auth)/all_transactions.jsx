920e02fc60f2918c8605aacfa1979732b8e6fa18
Leo1526 <mustafatanriyakul1@gmail.com>
2024-05-04 23:17:23 +0300

    alltransa

--- app/(auth)/all_transactions.jsx
+++ app/(auth)/all_transactions.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, FlatList, Modal, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { colors } from '../../design/themes';
+import { TextInput, Modal, Button } from 'react-native-paper';
 import TransactionItem from '../../components/TransactionItem';
 import RNPickerSelect from 'react-native-picker-select';
 
@@ -12,38 +13,72 @@ const AllTransactionsScreen = () => {
   const [filteredTransactions, setFilteredTransactions] = useState([]);
 
   const transactions = [
-    { id: '1', date: '01.05.2023', amount: -100, description: 'Kira ödemesi', sender: 'Ben', recipient: 'Kira Şirketi' },
-    { id: '2', date: '15.05.2023', amount: 200, description: 'Maaş', sender: 'İşveren', recipient: 'Ben' },
-    { id: '3', date: '20.05.2023', amount: -50, description: 'Hediye', sender: 'Ben', recipient: 'Arkadaş' },
-    { id: '4', date: '22.05.2023', amount: 150, description: 'Alacak', sender: 'Arkadaş', recipient: 'Ben' },
-    { id: '5', date: '25.05.2023', amount: -70, description: 'Market alışverişi', sender: 'Ben', recipient: 'Market' },
-    { id: '6', date: '02.06.2023', amount: -150, description: 'Elektrik Faturası', sender: 'Ben', recipient: 'Enerji Şirketi' },
-    { id: '7', date: '05.06.2023', amount: 300, description: 'Freelance İş', sender: 'Müşteri', recipient: 'Ben' },
-    { id: '8', date: '07.06.2023', amount: -45, description: 'Kitap Satın Alma', sender: 'Ben', recipient: 'Kitapçı' },
-    { id: '9', date: '10.06.2023', amount: -30, description: 'Sinema', sender: 'Ben', recipient: 'Sinema' },
-    { id: '10', date: '12.06.2023', amount: 90, description: 'Hediye', sender: 'Anne', recipient: 'Ben' }
+    { id: '1', date: '01.05.2023', type: 'Kira', amount: -100, description: 'Kira ödemesi', sender: 'Ben', recipient: 'Kira Şirketi' },
+    { id: '2', date: '15.05.2023', type: 'Maaş', amount: 200, description: 'Maaş', sender: 'İşveren', recipient: 'Ben' },
+    { id: '3', date: '20.05.2023', type: 'Diğer', amount: -50, description: 'Hediye', sender: 'Ben', recipient: 'Arkadaş' },
+    { id: '4', date: '22.05.2023', type: 'Diğer', amount: 150, description: 'Alacak', sender: 'Arkadaş', recipient: 'Ben' },
+    { id: '5', date: '25.05.2023', type: 'Yemek', amount: -70, description: 'Market alışverişi', sender: 'Ben', recipient: 'Market' },
+    { id: '6', date: '02.06.2023', type: 'Diğer', amount: -150, description: 'Elektrik Faturası', sender: 'Ben', recipient: 'Enerji Şirketi' },
+    { id: '7', date: '05.06.2023', type: 'Transfer', amount: 300, description: 'Freelance İş', sender: 'Müşteri', recipient: 'Ben' },
+    { id: '8', date: '07.06.2023', type: 'Diğer', amount: -45, description: 'Kitap Satın Alma', sender: 'Ben', recipient: 'Kitapçı' },
+    { id: '9', date: '10.06.2023', type: 'Eğlence', amount: -30, description: 'Sinema', sender: 'Ben', recipient: 'Sinema' },
+    { id: '10', date: '12.06.2023', type: 'Diğer', amount: 90, description: 'Hediye', sender: 'Anne', recipient: 'Ben' }
   ];
+  
 
   const applyFilter = () => {
     let filtered = transactions;
+  
+    // İşlem tipine göre filtreleme
     if (transactionType !== 'Tümü') {
       if (transactionType === 'Gider') {
         filtered = filtered.filter(transaction => transaction.amount < 0);
       } else if (transactionType === 'Gelir') {
         filtered = filtered.filter(transaction => transaction.amount >= 0);
       }
+      if (filtered.length === 0) {
+        Alert.alert('Uyarı', 'Filtreleme sonucunda hiçbir işlem bulunamadı.');
+        setModalVisible(false);
+        setFilterKeyword('');
+        return;
+      }
     }
+
+    if (transactionType !== 'Tümü') {
+      filtered = filtered.filter(transaction => transaction.type === transactionType);
+      if (filtered.length === 0) {
+        Alert.alert('Uyarı', 'Filtreleme sonucunda hiçbir işlem bulunamadı.');
+        setModalVisible(false);
+        setFilterKeyword('');
+        return;
+      }
+    }
+  
+    // İsim veya gönderici/alan kişiye göre filtreleme
     if (filterKeyword) {
+      const keywordLowerCase = filterKeyword.toLowerCase();
       filtered = filtered.filter(
         transaction =>
-          transaction.recipient.toLowerCase().includes(filterKeyword.toLowerCase()) ||
-          transaction.sender.toLowerCase().includes(filterKeyword.toLowerCase())
+          transaction.recipient.toLowerCase().includes(keywordLowerCase) ||
+          transaction.sender.toLowerCase().includes(keywordLowerCase)
       );
+  
+      // Eğer filtrelenmiş bir sonuç yoksa, filtered array'ini boş bırak
+      if (filtered.length === 0) {
+        Alert.alert('Uyarı', 'Filtreleme sonucunda hiçbir işlem bulunamadı.');
+        setModalVisible(false);
+        setFilterKeyword('');
+        return;
+      }
+      
     }
+    
+    
     setFilteredTransactions(filtered);
     setModalVisible(false);
+    setFilterKeyword('');
   };
-
+  
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
@@ -65,39 +100,48 @@ const AllTransactionsScreen = () => {
           keyExtractor={item => item.id}
         />
         <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <View style={styles.modalContainer}>
-            <View style={styles.modalContent}>
-              <Text style={styles.modalHeading}>Filtrele</Text>
-              <Text style={styles.inputHeading}>İsme göre filtrele</Text>
-              <TextInput
-                style={styles.input}
-                value={filterKeyword}
-                onChangeText={text => setFilterKeyword(text)}
-              />
-              <Text style={styles.inputHeading}>İşlem tipine göre filtrele</Text>
-              <RNPickerSelect
-                onValueChange={(value) => setTransactionType(value)}
-                items={[
-                  { label: 'Tümü', value: 'Tümü' },
-                  { label: 'Gider', value: 'Gider' },
-                  { label: 'Gelir', value: 'Gelir' },
-                ]}
-                style={{
-                    inputIOS: styles.input,
-                    inputAndroid: styles.input,
-                  }}
-              />
-              <TouchableOpacity style={styles.applyButton} onPress={applyFilter}>
-                <Text style={styles.applyButtonText}>Uygula</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </Modal>
+            visible={modalVisible}
+            onDismiss={() => setModalVisible(false)}
+            contentContainerStyle={styles.modalContent}
+          >
+            <Text style={styles.modalHeading}>Filtrele</Text>
+            <Text style={styles.inputHeading}>İsme göre filtrele</Text>
+            <TextInput
+              style={styles.input}
+              value={filterKeyword}
+              onChangeText={text => setFilterKeyword(text)}
+            />
+            <Text style={styles.inputHeading}>İşlem tipine göre filtrele</Text>
+            <RNPickerSelect
+              onValueChange={(value) => setTransactionType(value)}
+              items={[
+                { label: 'Tümü', value: 'Tümü' },
+                { label: 'Gider', value: 'Gider' },
+                { label: 'Gelir', value: 'Gelir' },
+              ]}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+            <RNPickerSelect
+              onValueChange={(value) => setTransactionType(value)}
+              items={[
+                { label: 'Tümü', value: 'Tümü' },
+                { label: 'Transfer', value: 'Transfer' },
+                { label: 'Yemek', value: 'Yemek' },
+                { label: 'Alışveriş', value: 'Alışveriş' },
+                { label: 'Diğer', value: 'Diğer' },
+              ]}
+              style={{
+                inputIOS: styles.input,
+                inputAndroid: styles.input,
+              }}
+            />
+            <Button mode="contained" style={styles.applyButton} onPress={applyFilter}>
+              Uygula
+            </Button>
+          </Modal>
       </View>
     </SafeAreaView>
   );
@@ -129,17 +173,19 @@ const styles = StyleSheet.create({
     color: colors.text,
     fontWeight: 'bold',
   },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  modal: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 20,
+    margin: 40,
+    borderRadius: 10,
   },
   modalContent: {
     backgroundColor: colors.primary,
     padding: 20,
     borderRadius: 8,
-    width: '80%',
+    alignItems: 'flex-start',
+    alignSelf: 'center',
+    width: '90%'
   },
   modalHeading: {
     fontSize: 18,
@@ -155,21 +201,16 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   input: {
-    backgroundColor: colors.text,
+    width: '100%',
+    marginBottom: 16,
+    backgroundColor: 'white', // SignIn'den alınan giriş alanı rengi
     borderRadius: 8,
-    padding: 10,
-    marginBottom: 10,
+    height: 50,
   },
   applyButton: {
+    marginTop: 10,
     backgroundColor: colors.secondary,
-    padding: 10,
-    borderRadius: 8,
-    alignSelf: 'flex-end',
-  },
-  applyButtonText: {
-    color: colors.text,
-    fontWeight: 'bold',
   },
 });
 
-export default AllTransactionsScreen
+export default AllTransactionsScreen;
