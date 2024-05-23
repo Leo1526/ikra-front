6409b329e443ccfca8c669bd36f621011c12185e
kadiraydogan <b21945834@cs.hacettepe.edu.tr>
2024-05-24 00:17:39 +0300

    kadir deneme

--- design/themes.js
+++ design/themes.js
@@ -1,14 +1,14 @@
 export const colors = {
-    primary: '#30475E',
-    secondary: '#F05454',
-    background: '#222831',
-    text: '#E8E8E8',
-    // Diğer renkler
-  };
+  primary: '#30475E',
+  secondary: '#F05454',
+  background: '#222831',
+  text: '#E8E8E8',
+  // Diğer renkler
+};
   
-  export const fonts = {
-    regular: 'Roboto-Regular',
-    bold: 'Roboto-Bold',
-    italic: 'Roboto-Italic',
-    // Diğer fontlar
-  };
+export const fonts = {
+  regular: 'Roboto-Regular',
+  bold: 'Roboto-Bold',
+  italic: 'Roboto-Italic',
+  // Diğer fontlar 
+};
